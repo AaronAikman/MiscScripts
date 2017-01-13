542f643c56292e988a59057c4eaf6c8513832e26
Aaron Johan <Aaron Johan>
2017-01-12 19:56:46 -0800

    A few simple algorithms

---
+++ Algorithms/ReturnThirdPointInLine.py
@@ -0,0 +1,24 @@
+# ReturnThirdPointInLine.py
+# Aaron Aikman
+# Returns a third point (v2) in a line based upon two v2's
+
+
+def plotPoint(x0, y0, x1, y1, x):
+    y = ((((y1 - y0) / (x1 - x0)) * (x - x0)) + y0)
+    result = y
+    return result
+
+
+while True:
+    print "\nEnter the time(x0,x1) and value (y0,y1) of two v2's,\n"
+    "followed by a third time(x) to find the value(y) \n"
+    "of a point on the plotted line.\nOr enter nothing to quit\n"
+    x0 = input("Enter x0: ")
+    y0 = input("Enter y0: ")
+    x1 = input("Enter x1: ")
+    y1 = input("Enter y1: ")
+    x = input("Enter  x: ")
+    if (x0 == "" or y0 == "" or x1 == "" or y1 == "" or x == ""):
+        break
+    tResult = plotPoint(x0, y0, x1, y1, x)
+    print "       y= %s" % (tResult)
