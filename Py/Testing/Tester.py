c8a9df335d3a28f7831d1f5eb37ccfd9bbe96907
Aaron <gamephreakus@yahoo.com>
2017-12-21 14:53:08 -0800

    vs commit adjustments

--- Py/Testing/Tester.py
+++ Py/Testing/Tester.py
@@ -14,3 +14,16 @@ print mult_all([3,1,-2,6])
 print mult_all([3,1,-2,6])
 
 print [int(x) for x in list('012')]
+
+str = "this is how I print things"
+print str
+etc = "this is what it feels like to type on this keyboard"
+
+print etc
+string = "pylint, y u complain about var names so much."
+
+def sum(a, b):
+    return reduce(lambda x,y: x+y, [a, b])
+
+
+print sum(2, 4)
